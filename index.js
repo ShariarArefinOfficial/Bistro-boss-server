f38eb2096acdc6797ab63923ee50860028492801
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-06-07 02:30:30 +0600

    Cart add , get and delete SetUp

--- index.js
+++ index.js
@@ -32,6 +32,30 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
+
+
+       // carts collection
+       app.get('/carts', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      });
+  
+      app.post('/carts', async (req, res) => {
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem);
+        res.send(result);
+      });
+
+      app.delete('/carts/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+      })
+
 
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
